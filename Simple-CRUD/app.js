8660af39c36b631d413b951f8f8d2e3b80cbc147
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-05-14 13:58:01 +0700

    :fire: 1st push

---
+++ Simple-CRUD/app.js
@@ -0,0 +1,92 @@
+////////////////////////////// Entry point setup///////////////////////
+var express = require('express');
+var mongoose = require('mongoose')
+
+var app = express();
+
+// Set up port
+var port = process.env.port || 8000
+
+// Config DB and schema
+var User = require('./models/user')
+var db_connection = require('./db') 
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+//////////////////////////////Routes definition//////////////////////
+app.get('/', (req, res) => {
+  res.redirect('/index')
+})
+
+app.get('/index', (req, res) => {
+  res.render('index')
+})
+
+// Initialize variables
+const username = 'test_username'
+const password = 'test_pass'
+var db = mongoose.connection
+
+/////////////////////////////////// Create ////////////////////////////////////
+app.post('/create', (req, res) => {
+
+  var new_user = new User({username: username, password: password})
+
+  new_user.save((err, collection) => {
+    if(err){
+      return res.send(err)
+    }
+    else{
+      res.send(new_user.username + " has been added")
+      console.log('create succedded')
+    }
+  })
+})
+
+////////////////////////////// READ /////////////////////////////
+app.post('/read', (req, res) => {
+
+  User.find({username: username}, (err, data) => {
+    if(err){
+      return console.log(err)
+    }
+    else{
+      res.json(data)
+      console.log('read succedded')
+    }
+  })
+})
+
+////////////////////////////// UPDATE /////////////////////////////
+// https://kb.objectrocket.com/mongo-db/how-to-use-the-mongoose-update-query-in-nodejs-1303
+app.post('/update', (req, res) => {
+
+  const filter = { username: username };
+  const update_query = { password: 'changed_password' };
+
+  // update function
+  User.updateOne(filter, update_query, (err,result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+})
+
+////////////////////////////// DELETE /////////////////////////////
+// https://kb.objectrocket.com/mongo-db/how-to-delete-documents-with-mongoose-235
+app.post('/delete', (req, res) => {
+
+  const filter = { username: username };
+
+  User.deleteOne(filter, function (err) {
+    if(err) console.log(err);
+    console.log("Successful deletion");
+  });
+})
+
+const server = app.listen(port, () => {
+  console.log('http://localhost:' + port)
+})
