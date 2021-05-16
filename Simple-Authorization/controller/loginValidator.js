4c960b3c6c1913a6dbbc836a3d1c26995969ef64
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-05-16 22:03:08 +0700

    :zap: update simple authorization

--- Simple-Authorization/controller/loginValidator.js
+++ Simple-Authorization/controller/loginValidator.js
@@ -3,15 +3,19 @@
  * Jump to routePass if session is set else jump back routeFail 
  */
 function routeJump(req, res, routePass, routeFail){
-    var user = req.session
+    var user = req.session.username
+    var quote = undefined
 
-    if(user.username){
-        var userSession = user.username
-        
+    if(user && user == 1){
+        quote = 'Casual User'
         // Always pass Object here when render, no String , no Number
         // If you dont convert it to the right type, you will meet this error in link down below
         // https://teamtreehouse.com/community/error-in-express-when-rendering-with-es6-shorthand
-        return res.render(routePass, {userSession})
+        return res.render(routePass, {user, quote: quote})
+    }
+    else if(user && user == 2){
+        quote = 'VIP User'
+        return res.render(routePass, {user, quote: quote})
     }
     return res.redirect(routeFail)
 }
