12ae5b96d9882add09c7929c8e91a6ce5dea70fc
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-05-28 22:08:06 +0700

    :zap: add prepend-append

---
+++ AJAX-Cases/Simple-prepend-append/index.js
@@ -0,0 +1,16 @@
+var express = require('express')
+
+var app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static("views"))
+
+app.get('/' , (req , res)=>{
+
+   res.render('index')
+
+})
+
+const server = app.listen(8080, () => {
+    console.log('http://localhost:8080')
+})
